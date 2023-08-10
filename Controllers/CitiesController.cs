477a7b24453e4942ba3a78c24f0ae8e3ace6b732
Marco Vavassori <mvavassori@abacusnext.com>
2023-08-10 15:15:37 +0100

    Bacis HttpGet endpoint and updated middleware

---
+++ Controllers/CitiesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController]
+    [Route("cities")]
+    public class CitiesController : ControllerBase
+    {
+        [HttpGet]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(
+                new List<object>
+                {
+                    new { id = 1, Name = "Verona" },
+                    new { id = 2, Name = "Bergamo" },
+                });
+        }
+    }
+}
