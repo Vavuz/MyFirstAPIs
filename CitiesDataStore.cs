37f377db29e255723e9201545423a22c905c4bf1
Marco Vavassori <mvavassori@abacusnext.com>
2023-08-10 15:38:08 +0100

    Added a data store

---
+++ CitiesDataStore.cs
@@ -0,0 +1,35 @@
+﻿using CityInfo.API.Models;
+
+namespace CityInfo.API
+{
+    public class CitiesDataStore
+    {
+        public List<CityDto> Cities { get; set; }
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
+
+        public CitiesDataStore()
+        {
+            Cities = new List<CityDto>()
+            {
+                new CityDto()
+                {
+                    Id = 1,
+                    Name = "Verona",
+                    Description = "City with the Arena."
+                },
+                new CityDto()
+                {
+                    Id = 2,
+                    Name = "Bergamo",
+                    Description = "City with high and low city."
+                },
+                new CityDto()
+                {
+                    Id = 3,
+                    Name = "Edinburgh",
+                    Description = "Capital of Scotland."
+                }
+            };
+        }
+    }
+}
