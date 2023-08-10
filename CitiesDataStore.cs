8523195c8a385016d4567e1de5cf2d52821b8597
Marco Vavassori <mvavassori@abacusnext.com>
2023-08-10 16:41:16 +0100

    Add a new model and controller for points of interest of a city

--- CitiesDataStore.cs
+++ CitiesDataStore.cs
@@ -15,19 +15,64 @@ namespace CityInfo.API
                 {
                     Id = 1,
                     Name = "Verona",
-                    Description = "City with the Arena."
+                    Description = "City with the Arena.",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 11,
+                            Name = "Arena di Verona",
+                            Description = "A big Arena built before the Colosseum."
+                        },
+                        new PointOfInterestDto()
+                        {
+                            Id = 12,
+                            Name = "Piazza Dante",
+                            Description = "A beautiful square that celebrates Dante Alighieri."
+                        }
+                    }
                 },
                 new CityDto()
                 {
                     Id = 2,
                     Name = "Bergamo",
-                    Description = "City with high and low city."
+                    Description = "City with high and low city.",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 21,
+                            Name = "Bergamo Tower",
+                            Description = "A tower next to the motorway with a Bergamo sign on it."
+                        },
+                        new PointOfInterestDto()
+                        {
+                            Id = 22,
+                            Name = "High City",
+                            Description = "The half of Bergamo build on a hill."
+                        }
+                    }
                 },
                 new CityDto()
                 {
                     Id = 3,
                     Name = "Edinburgh",
-                    Description = "Capital of Scotland."
+                    Description = "Capital of Scotland.",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 31,
+                            Name = "Edinburgh Castle",
+                            Description = "A beautiful castle that stands on the top of the Royal Mile."
+                        },
+                        new PointOfInterestDto()
+                        {
+                            Id = 32,
+                            Name = "Calton Hill",
+                            Description = "An hill with a lovely view on Edinburgh."
+                        }
+                    }
                 }
             };
         }
---
+++ Controllers/PointsOfInterestController.cs
@@ -0,0 +1,32 @@
+﻿using CityInfo.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/cities/{cityId}/pointsofinterest")]
+    [ApiController]
+    public class PointsOfInterestController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<PointOfInterestDto>> GetPointsOfInterest(int cityId)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            return (city == null ? NotFound() : Ok(city.PointsOfInterest));
+        }
+
+        [HttpGet("{pointOfInterestId}")]
+        public ActionResult<PointOfInterestDto> GetPointOfInterest(int cityId, int pointOfInterestId)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+
+            if (city != null)
+            {
+                var pointOfInterest = city.PointsOfInterest.FirstOrDefault(c => c.Id == pointOfInterestId);
+
+                if (pointOfInterest != null)
+                    return Ok(pointOfInterest);
+            }
+            return NotFound();
+        }
+    }
+}
